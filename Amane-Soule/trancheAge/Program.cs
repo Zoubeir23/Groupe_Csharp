b46a303014eafec61dc14007527f66fb1b08781d
Aman-Soule <amansoule9@gmail.com>
2025-11-28 12:53:40 +0000

    Ajout des exos sur Aman

---
+++ Amane-Soule/trancheAge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trancheAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration des variables
+            int age = 0;
+            bool rep = false;
+
+            do
+            {
+                // Recuperation de l'age
+                Console.WriteLine("Donner votre age : ");
+                age = int.Parse(Console.ReadLine());
+
+
+                // Determination de la tranche d'age
+                if (age < 15)
+                {
+                    Console.WriteLine("Vous etes mineur.");
+                }
+                else if (age < 35)
+                {
+                    Console.WriteLine("Vous etes adolescent ");
+                }
+
+                else
+                {
+                    Console.WriteLine("Vous etes adulte.");
+                }
+                Console.WriteLine("Voulez-vous recommencer ? (o/n) : ");
+                rep = Console.ReadLine().ToLower() == "o";
+            } while (rep);
+            
+
+
+        }
+    }
+}
