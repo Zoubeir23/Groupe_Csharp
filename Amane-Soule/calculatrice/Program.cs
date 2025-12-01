00af4eea6f8ae0365bb935e4bc0ec61d069a7149
Aman-Soule <amansoule9@gmail.com>
2025-12-01 15:25:13 +0000

    Calculatrice console

---
+++ Amane-Soule/calculatrice/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Calculatrice console");
+
+//Declaration des variables
+double a, b, resultat;
+char operateur;
+string choix;
+
+do
+{
+    //Recuperation des nombres et de l'operateur
+    Console.Write("Entrez le premier nombre : ");
+    a = double.Parse(Console.ReadLine());
+
+    Console.Write("Entrez le deuxième nombre : ");
+    b = double.Parse(Console.ReadLine());
+
+    Console.Write("Choisissez un opérateur (+, -, *, /) : ");
+    operateur = char.Parse(Console.ReadLine());
+
+    //Switch qui effectue l'operation (en fonction de l'operateur)
+    switch (operateur)
+    {
+        case '+':
+            resultat = a + b;
+            Console.WriteLine($"Résultat : {a} + {b} = {resultat}");
+            break;
+        case '-':
+            resultat = a - b;
+            Console.WriteLine($"Résultat : {a} - {b} = {resultat}");
+            break;
+        case '*':
+            resultat = a * b;
+            Console.WriteLine($"Résultat : {a} * {b} = {resultat}");
+            break;
+        case '/':
+            if (b != 0)
+            {
+                resultat = a / b;
+                Console.WriteLine($"Résultat : {a} / {b} = {resultat}");
+            }
+            else
+            {
+                Console.WriteLine("Erreur : division par zéro !");
+            }
+            break;
+        default:
+            Console.WriteLine("Opérateur invalide !");
+            break;
+    }
+
+    Console.Write("Voulez-vous recommencer ? (oui/non) : ");
+    choix = Console.ReadLine().ToLower();
+
+} while (choix == "oui");
+
+Console.WriteLine("Merci d'avoir utilisé la calculatrice !");
+
+
+Console.ReadLine();
